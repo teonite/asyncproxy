949ea7e8c1d3ddeda964b26e886bbb1c5df6489e
Kamil Chudy <kchudy@teonite.com>
2016-03-16 11:40:17 +0100

    Changed test to coroutine style

--- asyncproxy/tests.py
+++ asyncproxy/tests.py
@@ -2,8 +2,9 @@
 import time
 
 from tornado.curl_httpclient import CurlAsyncHTTPClient
+from tornado.httpclient import HTTPError
 from tornado.ioloop import IOLoop
-from tornado.testing import AsyncHTTPTestCase
+from tornado.testing import AsyncHTTPTestCase, gen_test
 
 from asyncproxy.asyncproxy import AsyncProxy
 
@@ -23,37 +24,38 @@ class AsyncProxyTestCase(AsyncHTTPTestCase):
     def get_http_client(self):
         return CurlAsyncHTTPClient(io_loop=self.io_loop)
 
+    @gen_test
     def test_proxy_without_range(self):
-        self.http_client.fetch(self.url, self.stop, proxy_host="localhost", proxy_port=self.get_http_port())
-        response = self.wait()
+        response = yield self.http_client.fetch(self.url, proxy_host="localhost", proxy_port=self.get_http_port())
         self.assertEqual(response.code, 200)
         self.assertEqual(int(response.headers["Content-Length"]), 84378)
 
+    @gen_test
     def test_proxy_with_range_header(self):
         headers = {
             "Range": "bytes=0-99"
         }
-        self.http_client.fetch(self.url, self.stop, headers=headers, proxy_host="localhost",
-                               proxy_port=self.get_http_port())
-        response = self.wait()
+        response = yield self.http_client.fetch(self.url, headers=headers, proxy_host="localhost",
+                                                proxy_port=self.get_http_port())
         self.assertEqual(response.code, 200)
         self.assertEqual(int(response.headers["Content-Length"]), 100)
 
+    @gen_test
     def test_proxy_with_range_param(self):
-        self.http_client.fetch("%s?range=bytes=0-99" % self.url, self.stop, proxy_host="localhost",
-                               proxy_port=self.get_http_port())
-        response = self.wait()
+        response = yield self.http_client.fetch("%s?range=bytes=0-99" % self.url, proxy_host="localhost",
+                                                proxy_port=self.get_http_port())
         self.assertEqual(response.code, 200)
         self.assertEqual(int(response.headers["Content-Length"]), 100)
 
+    @gen_test
     def test_proxy_range_conflict(self):
         headers = {
             "Range": "bytes=0-199"
         }
-        self.http_client.fetch("%s?range=bytes=0-99" % self.url, self.stop, headers=headers, proxy_host="localhost",
-                               proxy_port=self.get_http_port())
-        response = self.wait()
-        self.assertEqual(response.code, 416)
+
+        with self.assertRaises(HTTPError):
+            yield self.http_client.fetch("%s?range=bytes=0-99" % self.url, headers=headers,
+                                         proxy_host="localhost", proxy_port=self.get_http_port())
 
     def test_stats_uptime(self):
         time.sleep(1)
